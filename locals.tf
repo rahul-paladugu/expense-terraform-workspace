6c7be5139991587b476c1357ca69b06d475ced9f
rahul paladugu <rahulpaladugu96@gmail.com>
2026-05-01 11:24:57 +1200

    expense-terraform-workspaces

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  sg_tags = merge({Name = "sg-${terraform.workspace}-${local.common_name}"}, var.common_tags)
+  sg_tags = merge({Name = "sg-${var.project}-${terraform.workspace}"}, var.common_tags)
   common_name = "${terraform.workspace}-${var.project}-${var.region}"
   r53_common_name = "${local.common_name}.${var.r53_record_name}"
   public_r53_record = "${var.project}.${terraform.workspace}.${var.r53_record_name}"
